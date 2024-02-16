fada28538e849737a14bff53d2477d3619dba431
dhruvarora13 <dhruvarora2163@gmail.com>
2024-02-16 10:54:04 +0530

    Scaler Problem 5

---
+++ Problem/Problem5/problem5.js
@@ -0,0 +1,21 @@
+// Problem Statement: Create a function checkFileExtension(filePath, expectedExtension) that takes a file path and an expected file extension as input.
+//  The function should check if the file has the expected extension using the path module and print the result to the console.
+
+const path = require('path')
+
+function checkFileExtension(filePath, expectedExtension){
+
+    const mainFileExtension = path.extname(filePath)
+    if(mainFileExtension != expectedExtension){
+        console.log(`Expected Output: File does not have the expected extension. Expected: ${expectedExtension}, Actual: ${mainFileExtension}`)
+    }
+    else{
+        console.log(`Expected Output: File has the expected extension: ${expectedExtension} `);
+    }
+}
+
+checkFileExtension('test-files/file1.txt', '.txt');
+// Expected Output: File has the expected extension: .txt
+
+checkFileExtension('test-files/image.png', '.jpg');
+// Expected Output: File does not have the expected extension. Expected: .jpg, Actual: .png
